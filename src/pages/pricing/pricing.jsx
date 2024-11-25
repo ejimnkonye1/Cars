e7a90830d3b6563116354cf177396365874473a0
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-11-25 20:59:41 +0100

    pricing

---
+++ src/pages/pricing/pricing.jsx
@@ -0,0 +1,28 @@
+import bg from '../../images/bg_3.jpg'
+
+export const Pricing = () => {
+    return(
+        <div className="">
+           <section 
+            className="relative hero-wrap h-screen w-full bg-cover bg-center bg-top" 
+            style={{ backgroundImage: `url(${bg})` }} 
+            data-stellar-background-ratio="0.5"
+        >
+            <div className="absolute inset-0 bg-black opacity-30"></div>
+            <div className="container mx-auto flex flex-col h-full justify-end">
+                <div className="ftco-animate pb-5 pl-12">
+                    <p className="breadcrumbs text-white">
+                        <span className="mr-2">
+                            <a href="index.html" className="hover:text-[#01d28e]">Home <i className="ion-ios-arrow-forward"></i></a>
+                        </span>
+                        <span className="text-white">Pricing <i className="ion-ios-arrow-forward"></i></span>
+                    </p>
+                    <h1 className="mb-3 text-4xl text-white">Pricing</h1>
+                </div>
+            </div>
+        </section>
+        
+
+        </div>
+    )
+}
