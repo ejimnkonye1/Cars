4f682f9f40bcd8e94da461372711253282119019
ejimnkonye1 <143567665+ejimnkonye1@users.noreply.github.com>
2024-12-02 23:29:54 +0100

    contacts

--- src/pages/contact.jsx/contact.jsx
+++ src/pages/contact.jsx/contact.jsx
@@ -30,38 +30,38 @@ export const Contact = () => {
                         <div className="mb-5">
                             <div className="border w-full p-4 rounded mb-2 flex">
                                 <div className="icon mr-3">
-                                    <span className="icon-map-o"><FaMap /></span>
+                                    <span className=""><FaMap size='30px' className='text-[#007bff]' /></span>
                                 </div>
                                 <p>
-                                    <span className="font-semibold">Address:</span> 198 West 21th Street, Suite 721 New York NY 10016
+                                    <span className="font-semibold text-gray-500">Address:<br/></span > <span className='font-semibold text-md font-poppins text-black mb-0'>198 West 21th Street, Suite 721 New York NY 10016</span> 
                                 </p>
                             </div>
                         </div>
                         <div className="mb-5">
                             <div className="border w-full p-4 rounded mb-2 flex">
                                 <div className="icon mr-3">
-                                    <span className="icon-mobile-phone"><MdOutlinePhoneIphone />
+                                    <span className="icon-mobile-phone"><MdOutlinePhoneIphone size='20px' className='text-[#007bff]' />
                                     </span>
                                 </div>
                                 <p>
-                                    <span className="font-semibold">Phone:</span> <a href="tel://1234567920" className="text-blue-500">+ 1235 2355 98</a>
+                                    <span className="font-semibold text-gray-500">Phone:</span><br/> <a href="tel://1234567920" className="font-semibold text-md font-poppins text-black mb-0">+ 1235 2355 98</a>
                                 </p>
                             </div>
                         </div>
                         <div className="mb-5">
                             <div className="border w-full p-4 rounded mb-2 flex">
                                 <div className="icon mr-3">
-                                    <span className="icon-envelope-o"><IoMdMail />
+                                    <span className="icon-envelope-o"><IoMdMail size='20px' className='text-[#007bff]'  />
                                     </span>
                                 </div>
                                 <p>
-                                    <span className="font-semibold">Email:</span> <a href="mailto:info@yoursite.com" className="text-blue-500">info@yoursite.com</a>
+                                    <span className="font-semibold text-gray-500">Email:</span><br/> <a href="mailto:info@yoursite.com" className="font-semibold text-md font-poppins text-black mb-0">info@yoursite.com</a>
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div className="w-full lg:w-2/3 mb-5 lg:pl-5">
-                        <form action="#" className="bg-gray-100 p-5 rounded shadow-md">
+                        <form action="#" className="bg-gray-100 p-12 rounded shadow-md font-poppins">
                             <div className="mb-4">
                                 <input type="text" className="form-control w-full p-2 border rounded" placeholder="Your Name" />
                             </div>
@@ -81,16 +81,16 @@ export const Contact = () => {
                     </div>
                 </div>
                 <div className="flex justify-center">
-            <div className="w-full md:w-2/3">
-                <div className="relative bg-white w-full h-full rounded-lg shadow overflow-hidden"> 
-                    <iframe 
-                        className="absolute top-0 left-0  h-screen rounded-lg border-0"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
-                        frameBorder="0" 
-                        allowFullScreen 
-                        aria-hidden="false" 
-                        tabIndex="0">
-                    </iframe>
+            <div className="w-full md:w-3/3">
+                <div className="relative bg-white w-full h-[400px]  rounded-lg shadow overflow-hidden"> 
+                <iframe 
+    className="absolute top-0 left-0 w-full h-screen rounded-lg border-0" 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+    frameBorder="0" 
+    allowFullScreen 
+    aria-hidden="false" 
+    tabIndex="0">
+</iframe>
                 </div>
             </div>
         </div>
